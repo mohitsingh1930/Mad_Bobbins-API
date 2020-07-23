778d33cfe42d6f661c11ca44c80b434d9b31e418
Mohit Singh <mohitsingh1930@gmail.com>
2020-07-23 17:30:35 +0530

    Global error log added

--- app.js
+++ app.js
@@ -80,7 +80,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+	// set locals, only providing error in development
+	console.log("GLOBAL ERROR:::", err)
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
